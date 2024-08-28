0960547a7b79d7eaeb8364c105155e4402c38ed8
ionutprodea <147700111+ionutprodea@users.noreply.github.com>
2024-08-28 18:53:22 +0300

    Changed how navigation bar selection is updated to storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import AddTask from "./components/AddTask";
 import DeleteTask from "./components/DeleteTask";
 import ShowTasks from "./components/ShowTasks";
 import NavBar from "./components/NavBar";
 
 function App() {
-  const [selectedMenu, setSelectedMenu] = useState({
-    task: true,
-    add: false,
-    delete: false,
+  // Retrieve the state from localStorage or use the default state
+  const [selectedMenu, setSelectedMenu] = useState(() => {
+    const savedMenu = localStorage.getItem("selectedMenu");
+    return savedMenu
+      ? JSON.parse(savedMenu)
+      : { task: true, add: false, delete: false }; // Default state
   });
+  // Save the state to localStorage whenever it changes
+  useEffect(() => {
+    localStorage.setItem("selectedMenu", JSON.stringify(selectedMenu));
+  }, [selectedMenu]);
+
   return (
     <>
       <NavBar onSelectedMenu={setSelectedMenu} />
