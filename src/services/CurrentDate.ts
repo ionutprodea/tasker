83f21ef3d01da8d2cc2b94a19dc7ef41f8997c6e
Ionut Prodea <ionut.prodea@yahoo.com>
2024-09-05 12:05:13 +0300

    Fix for CurrentDate

--- src/services/CurrentDate.ts
+++ src/services/CurrentDate.ts
@@ -3,8 +3,8 @@ export function CurrentDate () {
     let day = date.getDate();
     let month = date.getMonth() + 1;
     let year = date.getFullYear();
+    if (day < 10 && month < 10) return `0${day}/0${month}/${year}`;
     if (day < 10) return `0${day}/${month}/${year}`;
     if (month < 10) return `${day}/0${month}/${year}`;
-    if (day < 10 && month < 10) return `0${day}/0${month}/${year}`
 }
 
