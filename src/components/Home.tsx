286be76e54dc1470f95775975e92fc555084b55a
ionutprodea <147700111+ionutprodea@users.noreply.github.com>
2024-09-11 21:54:21 +0300

    Updated Home component (added details toggle) Added Contact component

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -19,7 +19,12 @@ const Home = () => {
     setTasks(updatedTasks);
     localStorage.setItem("TASKER_TASKS", JSON.stringify(updatedTasks));
   };
-
+  const handleToggleDetails = (index: number) => {
+    const toggledTasks = tasks.map((task, i) =>
+      i === index ? { ...task, showDetails: !task.showDetails } : task
+    );
+    setTasks(toggledTasks);
+  };
   return (
     <div>
       <h1 className="m-5">Today's Tasks</h1>
@@ -32,18 +37,31 @@ const Home = () => {
                   className="list-group-item d-flex justify-content-between align-items-center"
                   key={task.date + task.task}
                 >
-                  <div className="d-flex justify-content-start align-items-center">
-                    <p className="my-1 me-3">{task.task}</p>
-                    <span className={task.importance}>
-                      {task.importance.toUpperCase()}
-                    </span>
-                    <span className="ms-3 task-date">{task.date}</span>
+                  <div>
+                    <div className="d-flex justify-content-start align-items-center">
+                      <p className="my-1 me-3">{task.task}</p>
+                      <span className={task.importance}>
+                        {task.importance.toUpperCase()}
+                      </span>
+                      <span className="ms-3 task-date">{task.date}</span>
+                      <div className="checkbox-wrapper-50">
+                        <input
+                          className="form-check-input shadow-none align-self-start mx-3 my-2 plus-minus"
+                          type="checkbox"
+                          name="task_description"
+                          onChange={() => handleToggleDetails(index)}
+                        />
+                      </div>
+                    </div>
+                    <p className="my-1 task-date-mobile">{task.date}</p>
+                    {task.showDetails && (
+                      <p className="my-1 task-details">{task.details}</p>
+                    )}
                   </div>
                   <input
-                    className="form-check-input shadow-none"
+                    className="form-check-input shadow-none align-self-start my-3"
                     type="checkbox"
                     name="task_status"
-                    id={task.task + task.date}
                     checked={task.status} // Checkbox will be checked if status is true
                     onChange={() => handleCheckboxChange(index)} // Toggle status on change
                   />
