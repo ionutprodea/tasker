b9b414dda7e498222fa8ba44325180cdd8788c85
Ionut Prodea <ionut.prodea@yahoo.com>
2024-09-19 13:58:05 +0300

    Updated text for About component

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,29 +4,32 @@ const About = () => {
       <h1 className="m-5">About</h1>
       <div className="m-5 centered-container">
         <section className="text-container">
-          <p className="text mb-5">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vel vero
-            obcaecati quo mollitia sit quae atque veniam hic odio molestiae qui
-            ipsum perferendis possimus labore, velit facilis magnam ipsam
-            corporis nemo. Debitis totam neque, architecto doloribus voluptatum,
-            inventore, ut optio quidem ducimus odit asperiores culpa explicabo!
-            Numquam neque, dolorum ut tempora excepturi quasi cum sapiente,
-            tempore non aliquid officiis culpa commodi pariatur recusandae
-            distinctio reprehenderit a nostrum.
+          <p className="text mb-4">
+            Tasker is a simple yet functional to-do application designed to help
+            you manage tasks efficiently. The app allows you to add new tasks,
+            delete completed or irrelevant ones, and view all your tasks in an
+            organized manner. It also includes sorting features to better manage
+            your list.
           </p>
-          <p className="text mb-5">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deserunt
-            labore dolorem voluptas eius voluptatum. Voluptatem nobis, corporis
-            placeat odit dolorem facere. Neque ipsum a nemo eveniet ullam
-            delectus ipsam accusamus mollitia impedit deserunt unde dolore
-            maiores nihil voluptatem necessitatibus, perferendis quas cumque
-            praesentium vitae! Pariatur officiis quod deleniti dolorum odit
-            ducimus dolores eveniet nobis. Ipsa assumenda tempora, recusandae
-            corrupti in, quasi veritatis dolores nobis, quibusdam eveniet
-            consequatur adipisci eum eos libero sint? Quos voluptatum ducimus id
-            harum optio doloremque quibusdam ipsa libero adipisci, corrupti
-            expedita a tenetur vitae et, vero dolores sequi animi nulla dolorum
-            esse? Molestiae voluptatum reprehenderit beatae.
+          <p className="text mb-4">
+            It is built with a mobile-first approach, Tasker is fully
+            responsive, offering a seamless experience on small and medium
+            display devices. The interface may feel a bit minimal on larger
+            screens
+          </p>
+          <p className="text mb-4">
+            As a frontend-only application, Tasker stores task entries locally
+            in your browser's memory (local storage). Please note that clearing
+            the local storage will result in the loss of all your tasks.
+          </p>
+          <p className="text mb-4">
+            Developed as a demo project for my portfolio, Tasker showcases my
+            skills in frontend development. For any enquiries, please use the
+            contact form or visit my{" "}
+            <a href="https://ionutprodea.ro/" className="website-link">
+              personal website
+            </a>
+            .
           </p>
         </section>
       </div>
