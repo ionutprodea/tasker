7935a853f544ff4f0d0a628b60652b5aa3f35402
ionutprodea <147700111+ionutprodea@users.noreply.github.com>
2024-10-31 20:25:27 +0200

    Fixed issue with CurrentDate function

--- src/services/CurrentDate.ts
+++ src/services/CurrentDate.ts
@@ -6,5 +6,6 @@ export function CurrentDate () {
     if (day < 10 && month < 10) return `0${day}/0${month}/${year}`;
     if (day < 10) return `0${day}/${month}/${year}`;
     if (month < 10) return `${day}/0${month}/${year}`;
+    return `${day}/${month}/${year}`;
 }
 
