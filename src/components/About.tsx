4c890f20e4d309328ea3e448635a2a652dc4e166
ionutprodea <147700111+ionutprodea@users.noreply.github.com>
2025-04-21 11:44:25 +0300

    Updated about page text

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -36,27 +36,28 @@ const About = () => {
             <section className="text-container">
               <p className="text mb-4">
                 Tasker is a simple yet functional to-do application designed to
-                help you manage tasks efficiently. The app allows you to add new
-                tasks, delete completed or irrelevant ones, and view all your
-                tasks in an organized manner. It also includes sorting features
-                to better manage your list.
+                help you manage your tasks efficiently. It allows you to add new
+                tasks, delete completed or irrelevant ones, and view your tasks
+                in an organized, sortable list for better control and clarity.
               </p>
               <p className="text mb-4">
-                It is built with a mobile-first approach, Tasker is fully
-                responsive, offering a seamless experience on small and medium
-                display devices. The interface may feel a bit minimal on larger
-                screens.
+                To use the app, you’ll need to create an account. Your tasks are
+                securely stored in a database via API integration, making them
+                accessible across devices and safely preserved even if you
+                switch browsers or clear your cache. Each account is limited to
+                50 task entries. If you need a higher limit, feel free to reach
+                out through the contact page.
               </p>
               <p className="text mb-4">
-                As a frontend-only application, Tasker stores task entries
-                locally in your browser's memory (local storage). Please note
-                that clearing the local storage will result in the loss of all
-                your tasks.
+                Built with a mobile-first approach, Tasker is fully responsive,
+                delivering a smooth experience on small and medium display
+                devices. The interface remains clean and minimal, though it may
+                feel a bit spacious on larger screens.
               </p>
               <p className="text mb-4">
                 Developed as a demo project for my portfolio, Tasker showcases
-                my skills in frontend development. For any enquiries, please use
-                the contact form or visit my{" "}
+                my web development skills. For any enquiries, please use the
+                contact form or visit my{" "}
                 <a href="https://ionutprodea.ro/" className="website-link">
                   personal website
                 </a>
